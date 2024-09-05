4328cd71e88e3684154a97c7a4fbab915c76fb1a
mcyinsz <yihan_yin@163.com>
2024-09-05 11:07:35 +0800

    modified:   .gitignore 	modified:   detect.py

---
+++ detect.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+class DetectMethod():
+
+    def __init__(self,img_array:np.ndarray):
+        self.img_array=img_array
+
+    def HOGE(self):
+        ''''
+            use HOGE 
+        '''
