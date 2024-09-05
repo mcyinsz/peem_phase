0eb238d7c37d4c68ae895107eb421aaaaae8d749
mcyinsz <yihan_yin@163.com>
2024-09-05 11:23:28 +0800

    Changes to be committed: 	modified:   detect.py
    Changes not staged for commit:
    	modified:   detect.py
    	modified:   file.py
    	modified:   helper.py
    
    Untracked files:
    	tempCodeRunnerFile.py

--- detect.py
+++ detect.py
@@ -9,5 +9,11 @@ class DetectMethod():
 
     def HOGE(self):
         ''''
-            use HOGE and K-means to get waveguide
+            use HOGE and K-means to get waveguide line
         '''
+
+        image_blurred = cv2.GaussianBlur(self.img_array,ksize=(9,9),sigmaX=-1,sigmaY=-1)
+        edges = cv2.Canny(image_blurred,threshold1=50,threshold2=100)
+        
+
+
