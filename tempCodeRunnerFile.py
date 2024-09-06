26adb0da32eed70557b8a8333c8670f7d6ddb488
mcyinsz <yihan_yin@163.com>
2024-09-06 15:35:55 +0800

    deleted:    tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-        # for row in range(horizon_img.shape[0]):
-        #     if np.mean(horizon_img[row])>0.25:
-        #         print(row,np.mean(horizon_img[row]))
-        #         plt.plot(horizon_img[row])
-        #         plt.show()
